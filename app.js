4650120829726c0d2dcf59b0c25b0ab189c4b58c
asfiasidd <126721258+asfiasidd@users.noreply.github.com>
2023-05-17 00:39:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function getnum(num) {
+    console.log(num)
+    document.getElementById('result').value += num;
+}
+function clearScr() {
+   document.getElementById('result').value = "";
+}
+// function removenum() {
+   
+//     var res = document.getElementById('result').value;
+//     document.getElementById('result').value = document.getElementById('result').value.slice(0,res.length-1);
+
+// }
+function getans(){
+    document.getElementById('result').value = eval(document.getElementById('result').value);
+    console.log(document.getElementById('result').value);
+}
+
